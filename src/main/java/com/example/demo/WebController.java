1fea1287bafeda330a99ae82edb44ce3a6eb86ac
Kevin Liu <kevinliu970123@gmail.com>
2024-10-29 13:10:47 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        // 传递数据到网页 (可选)
+        model.addAttribute("message", "fdf的网站");
+
+        // 返回名为 "home" 的模板
+        return "home";
+    }
+}
